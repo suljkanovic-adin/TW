a4cb7d26fd71da454303fb1def5cf24b827d2546
suljkanovic-adin <91219356+suljkanovic-adin@users.noreply.github.com>
2024-02-03 09:48:58 +0100

    Update fa.js

--- Farm_Assist/fa.js
+++ Farm_Assist/fa.js
@@ -29,197 +29,196 @@
 // ==/UserScript==
 
 function random(superior,inferior) {
-    let numPosibilidades = superior - inferior;
-    let aleat = Math.random() * numPosibilidades;
-    return Math.round(parseInt(inferior) + aleat);
+let numPosibilidades = superior - inferior;
+let aleat = Math.random() * numPosibilidades;
+return Math.round(parseInt(inferior) + aleat);
 }
 
 function verifyRecaptcha(){
-    if($(".g-recaptcha").length){
-        console.log("recaptcha");
-        $("div.recaptcha-checkbox-checkmark").click();
-    }
+if($(".g-recaptcha").length){
+    console.log("recaptcha");
+    $("div.recaptcha-checkbox-checkmark").click();
+}
 }
 
 setTimeout(verifyRecaptcha, random(2000,10000));
 
 function getPage(i, pages) {
-    var sitter = "";
-    if (window.top.game_data.player.sitter != "0") {
-        sitter = "t=" + window.top.game_data.player.id + "&";
-    }
-    var link = ["https://" + window.location.host + "/game.php?" + sitter + "village=", "&screen=am_farm"];
-    if (i < pages) {
-        //         changeHeader(filter_41 + " " + (i + 1) + "/" + pages + " <img src='graphic/throbber.gif' height='24' width='24'></img>");
-        var url = link[0] + window.top.game_data.village.id + "&order=distance&dir=asc&Farm_page=" + i + "&screen=am_farm";
-        window.top.$.ajax({
-            type: 'GET', url: url, dataType: "html", error: function (xhr, statusText, error) {
-                console.log("Get page failed with error: " + error);
-            }, success: function (data) {
-                window.top.$('#plunder_list tr', data).slice(2).each(function () {
-                    window.top.$('#plunder_list tr:last').after("<tr>" + window.top.$(this).html() + "</tr>");
-                });
-                setTimeout(function () {
-                    getPage(i + 1, pages);
-                }, 1);
-            }
-        });
-    } else {
-    }
+var sitter = "";
+if (window.top.game_data.player.sitter != "0") {
+    sitter = "t=" + window.top.game_data.player.id + "&";
+}
+var link = ["https://" + window.location.host + "/game.php?" + sitter + "village=", "&screen=am_farm"];
+if (i < pages) {
+    //         changeHeader(filter_41 + " " + (i + 1) + "/" + pages + " <img src='graphic/throbber.gif' height='24' width='24'></img>");
+    var url = link[0] + window.top.game_data.village.id + "&order=distance&dir=asc&Farm_page=" + i + "&screen=am_farm";
+    window.top.$.ajax({
+        type: 'GET', url: url, dataType: "html", error: function (xhr, statusText, error) {
+            console.log("Get page failed with error: " + error);
+        }, success: function (data) {
+            window.top.$('#plunder_list tr', data).slice(2).each(function () {
+                window.top.$('#plunder_list tr:last').after("<tr>" + window.top.$(this).html() + "</tr>");
+            });
+            setTimeout(function () {
+                getPage(i + 1, pages);
+            }, 1);
+        }
+    });
+} else {
+}
 }
 
 getPage(1, 1);
 
 setTimeout(function(){
 
-    var FARM_TITLE = "Farm",
-        STOP_TITLE = "Stop",
-        CHEAT_MODE = " cheat",
-        autoPause = false,
-        //     cheat = sessionStorage.cheat === undefined ? false : true,
-        cheat = true,
-        userPause = false,
+var FARM_TITLE = "Farm",
+    STOP_TITLE = "Stop",
+    CHEAT_MODE = " cheat",
+    autoPause = false,
+    //     cheat = sessionStorage.cheat === undefined ? false : true,
+    cheat = true,
+    userPause = false,
 //         userPause = sessionStorage.pause === undefined ? false : true,
-        s1;
-    document.onkeydown = checkPause;
+    s1;
+document.onkeydown = checkPause;
 
-    function startFarm(icon) {
-        var i = sessionStorage.index === undefined ? 0 : sessionStorage.index,
-            tableFarm = $("#plunder_list").find(".farm_icon_" + icon);
-        s1 = setInterval(function() {
-            if (!autoPause && !userPause)
+function startFarm(icon) {
+    var i = sessionStorage.index === undefined ? 0 : sessionStorage.index,
+        tableFarm = $("#plunder_list").find(".farm_icon_" + icon);
+    s1 = setInterval(function() {
+        if (!autoPause && !userPause)
+        {
+            var button = tableFarm.eq(i++);
+            button.trigger('click');
+            if (button.css("opacity") == "0.5" || checkUnit())
             {
-                var button = tableFarm.eq(i++);
-                button.trigger('click');
-                if (button.css("opacity") == "0.5" || checkUnit())
-                {
-                    sessionStorage.index = --i;
-                    reloadPage();
-                }
-                else if (i >= tableFarm.length)
-                {
-                    delete sessionStorage.index;
-                    reloadPage();
-                }
-                else
-                    button.closest("tr").remove();
+                sessionStorage.index = --i;
+                reloadPage();
             }
-        }, Math.floor(Math.random() * (230 - 200 + 1)) + 230);
-    }
-
-    function checkUnit() {
-        var noUnit = $(".error");
-        if (noUnit.length && noUnit.text().indexOf("5") == -1)
-            return true;
-        else
-            return false;
-    }
-
-    function checkPause(e)
-    {
-        switch(e.keyCode)
-        {
-            case 70:
-                if (!userPause)
-                {
-                    isWorking(true);
-                    sessionStorage.pause = userPause = true;
-                }
-                else
-                {
-                    userPause = false;
-                    isWorking(false);
-                    delete sessionStorage.pause;
-                }
-                break;
-            case 67:
-                if (sessionStorage.cheat)
-                {
-                    cheat = false;
-                    delete sessionStorage.cheat;
-                }
-                else
-                    sessionStorage.cheat = cheat = true;
-                updateTitle();
-                break;
-        }
-    }
-
-    function isWorking(status)
-    {
-        autoPause = status;
-        updateTitle();
-    }
-
-    function reloadPage() {
-        if (!userPause)
-        {
-            var changeVillage = $("#village_switch_right");
-            clearInterval(s1);
-            if (changeVillage.length)
-                location.href = changeVillage.attr("href");
+            else if (i >= tableFarm.length)
+            {
+                delete sessionStorage.index;
+                reloadPage();
+            }
             else
-                location.reload(true);
+                button.closest("tr").remove();
         }
-    }
-
-    $(window).blur(function() {
-        if (!userPause && !cheat)
-            isWorking(true);
-    });
-
-    $(window).focus(function() {
-        if (!userPause && !cheat)
-            isWorking(false);
-    });
-
-    function createOptionWindow() {
-    // Create a simple HTML window with radio buttons
-    var optionWindow = `
-        <div id="optionWindow" style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); padding: 20px; background: white; border: 1px solid #ccc; z-index: 9999;">
-            <label><input type="radio" name="farmOption" value="a" checked> Option A</label><br>
-            <label><input type="radio" name="farmOption" value="b"> Option B</label><br>
-            <label><input type="radio" name="farmOption" value="c"> Option C</label><br>
-            <button onclick="closeOptionWindow()">Close</button>
-        </div>
-    `;
-
-    // Append the window to the body
-    $('body').append(optionWindow);
+    }, Math.floor(Math.random() * (230 - 200 + 1)) + 230);
 }
 
-    function closeOptionWindow() {
-    // Close the option window
-    $('#optionWindow').remove();
+function checkUnit() {
+    var noUnit = $(".error");
+    if (noUnit.length && noUnit.text().indexOf("5") == -1)
+        return true;
+    else
+        return false;
 }
 
-    
-
-    function execute() {
-        // Show the option window after the DOM is ready
-        $(document).ready(function () {
-            createOptionWindow();
-        });
-    
-        // Execute the script based on the selected option
-        setTimeout(function () {
-            var selectedOption = $("input[name='farmOption']:checked").val();
-            console.log("Selected option:", selectedOption);
-            startFarm(selectedOption);
-            closeOptionWindow();
-        }, 100);
+function checkPause(e)
+{
+    switch(e.keyCode)
+    {
+        case 70:
+            if (!userPause)
+            {
+                isWorking(true);
+                sessionStorage.pause = userPause = true;
+            }
+            else
+            {
+                userPause = false;
+                isWorking(false);
+                delete sessionStorage.pause;
+            }
+            break;
+        case 67:
+            if (sessionStorage.cheat)
+            {
+                cheat = false;
+                delete sessionStorage.cheat;
+            }
+            else
+                sessionStorage.cheat = cheat = true;
+            updateTitle();
+            break;
     }
+}
 
-    function updateTitle() {
-        if (autoPause || userPause)
-            document.title = STOP_TITLE;
+function isWorking(status)
+{
+    autoPause = status;
+    updateTitle();
+}
+
+function reloadPage() {
+    if (!userPause)
+    {
+        var changeVillage = $("#village_switch_right");
+        clearInterval(s1);
+        if (changeVillage.length)
+            location.href = changeVillage.attr("href");
         else
-            document.title = FARM_TITLE;
-
-        if (cheat)
-            document.title += CHEAT_MODE;
+            location.reload(true);
     }
+}
 
-    execute();
+$(window).blur(function() {
+    if (!userPause && !cheat)
+        isWorking(true);
+});
+
+$(window).focus(function() {
+    if (!userPause && !cheat)
+        isWorking(false);
+});
+
+function createOptionWindow() {
+    // Check if the option window already exists
+    if ($("#farmOptionWindow").length === 0) {
+        // Create the option window
+        var optionWindow = $("<div id='farmOptionWindow' style='position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background: white; padding: 20px; border: 2px solid #333;'></div>");
+        optionWindow.append("<label><input type='radio' name='farmOption' value='a' checked> Option A</label><br>");
+        optionWindow.append("<label><input type='radio' name='farmOption' value='b'> Option B</label><br>");
+        optionWindow.append("<label><input type='radio' name='farmOption' value='c'> Option C</label><br>");
+        optionWindow.append("<button onclick='closeOptionWindow()'>Close</button>");
+    
+        // Append the option window to the body
+        $("body").append(optionWindow);
+    }
+}
+
+function closeOptionWindow() {
+    $("#farmOptionWindow").remove();
+}
+
+
+
+function execute() {
+    // Show the option window after the DOM is ready
+    $(document).ready(function () {
+        createOptionWindow();
+    });
+
+    // Execute the script based on the selected option
+    setTimeout(function () {
+        var selectedOption = $("input[name='farmOption']:checked").val();
+        console.log("Selected option:", selectedOption);
+        startFarm(selectedOption);
+        closeOptionWindow();
+    }, 100);
+}
+
+function updateTitle() {
+    if (autoPause || userPause)
+        document.title = STOP_TITLE;
+    else
+        document.title = FARM_TITLE;
+
+    if (cheat)
+        document.title += CHEAT_MODE;
+}
+
+execute();
 
 }, random(500, 700));
